7fb3ee0d1ea29206b625472b98b13052ca9eebaa
Teixeira <gt3ixeira@gmail.com>
2021-02-13 20:42:15 -0300

    update style views

--- eventApp/resources/views/welcome.blade.php
+++ eventApp/resources/views/welcome.blade.php
@@ -7,10 +7,13 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
+
     <title>Laravel</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="./js/scripts.js"></script>
 </head>
 
 <body>
