453f04631e276b4f26650a607ec0710b57c29aea
Teixeira <gt3ixeira@gmail.com>
2021-02-13 13:12:58 -0300

    update views login

--- eventApp/resources/views/welcome.blade.php
+++ eventApp/resources/views/welcome.blade.php
@@ -1,31 +1,46 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    </head>
-    <body>
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="navbar navbar-expand-lg navbar-light " 
+    style="height:40px; margin-top: 10px; margin-right: 5px">
+        <form class="d-flex">
+            <div class="position-absolute top-0 end-0">
+                @if (Route::has('login'))
+                <div class="">
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+                    <a href="{{ url('/dashboard') }}" class="btn btn-outline-success">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                        @endif
+                    <a href="{{ route('login') }}" class="btn btn-outline-success">Login</a>
+                    @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="btn btn-outline-success">Register</a>
+                    @endif
                     @endauth
                 </div>
-            @endif
-    <h1>Hello Word</h1>
-    </body>
+                @endif
+            </div>
+        </form>
+    </div>
+
+    <div class="container">
+        <h1>Hello Word</h1>
+
+        @if(10>5)
+        <h1>è verdade</h1>
+        @endif
+    </div>
+</body>
+
 </html>
