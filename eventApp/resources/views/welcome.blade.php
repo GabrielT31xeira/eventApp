4773e17d8122fdb3f37ff725f0ef2f9ce0e59ef4
Teixeira <gt3ixeira@gmail.com>
2021-02-15 07:27:00 -0300

    update styles

--- eventApp/resources/views/welcome.blade.php
+++ eventApp/resources/views/welcome.blade.php
@@ -1,49 +1,39 @@
+@extends('layouts.main')
+
+@section('title','HDC Events')
+
+@section('content')
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./css/style.css">
-    <script src="./js/scripts.js"></script>
+
+    <title>@yield('title')</title>
 </head>
+<div class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="logs-position">
+        @if (Route::has('login'))
+        @auth
+        <a href="{{ url('/dashboard') }}" class="btn btn-outline-success" data-placement="right">Dashboard</a>
+        @else
+        <a href="{{ route('login') }}" class="btn btn-outline-success" data-toggle="tooltip" data-placement="right">Login</a>
+        @if (Route::has('register'))
+        <a href="{{ route('register') }}" class="btn btn-outline-success" data-placement="right">Register</a>
+        @endif
+        @endauth
 
-<body>
-    <div class="navbar navbar-expand-lg navbar-light " 
-    style="height:40px; margin-top: 10px; margin-right: 5px">
-        <form class="d-flex">
-            <div class="position-absolute top-0 end-0">
-                @if (Route::has('login'))
-                <div class="">
-                    @auth
-                    <a href="{{ url('/dashboard') }}" class="btn btn-outline-success">Dashboard</a>
-                    @else
-                    <a href="{{ route('login') }}" class="btn btn-outline-success">Login</a>
-                    @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="btn btn-outline-success">Register</a>
-                    @endif
-                    @endauth
-                </div>
-                @endif
-            </div>
-        </form>
-    </div>
-
-    <div class="container">
-        <h1>Hello Word</h1>
-
-        @if(10>5)
-        <h1>è verdade</h1>
         @endif
     </div>
-</body>
+</div>
+
+
+<div class="container">
+</div>
 
 </html>
+@endsection
