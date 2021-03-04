12d1d6a59b5f989293a3d4eaa3ce1ff0aac35946
Teixeira <gt3ixeira@gmail.com>
2021-03-03 22:16:00 -0300

    update login and register

--- eventApp/routes/web.php
+++ eventApp/routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function (){
     return view('welcome');
 });
 Route::get('/eventos', [EventController::class ,'index']);
-Route::get('/eventos/criar', [EventController::class ,'create']);
+Route::get('/eventos/criar', [EventController::class ,'create'])->middleware('auth');
 Route::get('/eventos/{id}', [EventController::class ,'show']);
 Route::post('/eventos', [EventController::class ,'store']);
 
