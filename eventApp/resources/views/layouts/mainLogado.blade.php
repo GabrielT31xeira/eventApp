ddf8b0263b18db4f3af7172d08e0bb3d4e4af2cc
Teixeira <gt3ixeira@gmail.com>
2021-03-16 12:29:14 -0300

    styles update

--- eventApp/resources/views/layouts/mainLogado.blade.php
+++ eventApp/resources/views/layouts/mainLogado.blade.php
@@ -6,17 +6,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://bootswatch.com/4/litera/bootstrap.min.css">
-    <link href="/css/criarevento/index.css" rel="stylesheet"> 
+    <link href="/css/criarevento/index.css" rel="stylesheet">
 
     <title>@yield('title')</title>
 </head>
 
 <body>
     <div class="navbar navbar-expand-lg navbar-light bg-primary">
-        <img src="/img/mars_planet_space_icon_176329.png" class="mars" title="hello" style="width: 3%;"/>
+        <img src="/img/mars_planet_space_icon_176329.png" class="mars" title="hello" style="width: 3%;" />
         <a href="/" class="navbar-brand">HDC Events</a>
 
-        <a href="/eventos/criar" class="btn btn-success" style="margin-left: 970px;">Criar Evento</a>        
+        <div style="margin-left: 900px; ">
+            <a href="/eventos/criar" class="btn btn-success">Criar Evento</a>
+            <a href="/eventos" class="btn btn-success">Ver Eventos</a>
+        </div>
     </div>
     <header>
 
